715cf2b77cf6d0f93d44b8ddb16f2aae3435c372
thomasbiv <bivinsthomas@gmail.com>
2021-07-14 11:02:50 -0400

    Removal of playlink (currently unneeded)

--- audioFunctions.py
+++ audioFunctions.py
@@ -176,49 +176,9 @@ class audioFunctions(commands.Cog):
         await ctx.guild.voice_client.disconnect()
         await ctx.send("I have left the voice channel.")
 
-        
+    
 
-    @commands.command(name="playlink", help=" - Play audio off of YouTube using a link. Also adds to queue.")
-    @commands.has_role('Vibe Master')
-    async def playlink(self, ctx, url: str):
-        if (ctx.author.voice):
-            if ctx.guild.id not in multiServerQueue:
-                multiServerQueue[ctx.guild.id] = []
-            if not (ctx.voice_client):
-                channel = ctx.message.author.voice.channel
-                voice = await channel.connect()
-                ydl_opts = {
-                    'quiet': True,
-                    'skip_download': True,
-                }
-                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                    info = ydl.extract_info(url)
-
-                multiServerQueue[ctx.guild.id].append(info['title'])
-                await ctx.send('***Selection added to queue!*** :ok_hand:')
-                temp = self.bot.get_command(name='playq')
-                await temp.callback(self, ctx)
-
-            else:
-                voice = ctx.guild.voice_client
-                ydl_opts = {
-                    'quiet': True,
-                    'skip_download': True,
-                }
-                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                    info = ydl.extract_info(url)
-
-                multiServerQueue[ctx.guild.id].append(info['title'])
-                await ctx.send('***Selection added to queue!*** :ok_hand:')
-                if not (voice.is_playing() or voice.is_paused()):
-                    temp = self.bot.get_command(name='playq')
-                    await temp.callback(self, ctx)
-                
-
-        else:
-            await ctx.send("You are not in a voice channel, you must be in a voice channel to run this command.")
-
-    @commands.command(name="play", help=" - Play audio off of YouTube using keywords. Also adds to queue.")
+    @commands.command(name="play", help=" - Play audio off of YouTube using keywords or url. Also adds to queue.")
     @commands.has_role('Vibe Master')
     async def play(self, ctx, *, search):
         query_string = urllib.parse.urlencode({
